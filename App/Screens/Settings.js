9bb043ca6a0bedee8a8930cd7b9141c4bc1415ce
Francesca <fla5029@psu.edu>
2021-03-24 00:20:30 -0400

    Storage

--- App/Screens/Settings.js
+++ App/Screens/Settings.js
@@ -1,19 +1,76 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Picker } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native';
 import Checkbox from 'expo-checkbox';
 //https://docs.expo.io/versions/latest/sdk/checkbox/
 import Logo from '../Components/Logo'
-import { useState } from 'react';
-import Picker from '@react-native-picker/picker'
+import { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App ({Navigation}) {
-    const [flowerColor, setFlowerColor] = useState('');
-    const [fruitColor, setFruitColor] = useState('');
+    const [flowerColor, setFlowerColor] = useState();
+    const [fruitColor, setFruitColor] = useState();
     const [isVegChecked, setVegChecked] = useState(false);
     const [isEdibleChecked, setEdibleChecked] = useState(false);
-    const [selectedValue, setSelectedValue] = useState("java");
+
+    const setFlowerColorFromStorage = (flowerColor_string) => {
+      setFlowerColor(JSON.parse(flowerColor_string));
+      console.log("Set flower Color from Storage");
+    }
+    
+
+    const storeFlowerColor = async (newValue) => {
+      try {
+        await AsyncStorage.setItem('flowerColor', JSON.stringify(newValue))
+      } catch (e) {
+        console.error(e)
+      }
+    };
+
+    const readFlowerColor = async () => {
+      try {
+        const storage_flowerColor = await AsyncStorage.getItem('flowerColor');
+        if (storage_flowerColor !== null) {
+          console.log("Found flower Color")
+          setFlowerColorFromStorage(storage_flowerColor);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    //Persistent Storage Fruit Color
+    const setFruitColorFromStorage = (fruitColor_string) => {
+      setFruitColor(JSON.parse(fruitColor_string));
+      console.log("Set fruit Color from Storage");
+    }
+    
+
+    const storeFruitColor = async (newValue) => {
+      try {
+        await AsyncStorage.setItem('fruitColor', JSON.stringify(newValue))
+      } catch (e) {
+        console.error(e)
+      }
+    };
+
+    const readFruitColor = async () => {
+      try {
+        const storage_fruitColor = await AsyncStorage.getItem('fruitColor');
+        if (storage_fruitColor !== null) {
+          console.log("Found fruit Color")
+          setFruitColorFromStorage(storage_fruitColor);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    useEffect(() => {
+      readFlowerColor();
+      readFruitColor();
+    }, [])
 
     return (
       <SafeAreaView style = {styles.container}>
@@ -36,16 +93,31 @@ export default function App ({Navigation}) {
             <Text style= {styles.textStyle}> Edible </Text>
           </View>
 
-          <View style= {styles.section}></View>
-          <Text>Flower Color</Text>
+        <View style= {styles.section}>
+          <Text style= {styles.textStyle}>Flower Color:</Text>
           <Picker
-        selectedValue={selectedValue}
-        style={{ height: 50, width: 150 }}
-        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-      >
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
+            flowerColor={flowerColor}
+            style={{ height: 50, width: 150 }}
+            onValueChange={(itemValue, itemIndex) => storeFlowerColor(itemValue)}
+          >
+              <Picker.Item label="Yellow" value="yellow" />
+              <Picker.Item label="Orange" value="orange" />
+              <Picker.Item label="Red" value="red" />
       </Picker>
+      </View>
+
+      <View style = {styles.section}>
+        <Text style = {styles.textStyle}>Fruit Color:</Text>
+        <Picker
+            fruitColor={fruitColor}
+            style={{ height: 50, width: 150 }}
+            onValueChange={(itemValue, itemIndex) => storeFruitColor(itemValue)}
+          >
+              <Picker.Item label="Yellow" value="yellow" />
+              <Picker.Item label="Orange" value="orange" />
+              <Picker.Item label="Red" value="red" />
+        </Picker>
+      </View>
 
       </SafeAreaView>
 
@@ -65,7 +137,7 @@ export default function App ({Navigation}) {
         },
         
         section: {
-          marginBottom: 200, 
+          marginBottom: 100, 
           flexDirection: 'row',
           alignItems: 'center',
         },
